60e113f1dff139b8b9d49850448ee4cc5d493d9c
Adam Carpenter <adam.carpenter@icd-tech.com>
2021-06-22 12:58:40 -0600

    add in force chart

--- src/app/layout/Chart.tsx
+++ src/app/layout/Chart.tsx
@@ -5,8 +5,15 @@ import { timeFormat } from 'd3-time-format';
 
 // @ts-ignore
 import { ChartCanvas, Chart } from 'react-stockcharts';
-// @ts-ignore
-import { BarSeries, AreaSeries, CandlestickSeries, LineSeries } from 'react-stockcharts/lib/series';
+import {
+  BarSeries,
+  AreaSeries,
+  CandlestickSeries,
+  AlternatingFillAreaSeries,
+  LineSeries,
+  StraightLine
+  // @ts-ignore
+} from 'react-stockcharts/lib/series';
 // @ts-ignore
 import { XAxis, YAxis } from 'react-stockcharts/lib/axes';
 import {
@@ -20,9 +27,9 @@ import {
 // @ts-ignore
 import { discontinuousTimeScaleProvider } from 'react-stockcharts/lib/scale';
 // @ts-ignore
-import { OHLCTooltip, MovingAverageTooltip } from 'react-stockcharts/lib/tooltip';
+import { OHLCTooltip, MovingAverageTooltip, SingleValueTooltip } from 'react-stockcharts/lib/tooltip';
 // @ts-ignore
-import { ema, sma } from 'react-stockcharts/lib/indicator';
+import { ema, sma, forceIndex } from 'react-stockcharts/lib/indicator';
 // @ts-ignore
 import { last } from 'react-stockcharts/lib/utils';
 
@@ -31,8 +38,8 @@ const ChartView = (props: any) => {
   console.log(props);
   // @ts-ignore
   const { data: initialData } = props;
-  const width = 1000;
-  const ratio = 3;
+  const width = 1200;
+  const ratio = 2;
   const type = 'hybrid';
 
   const ema20 = ema()
@@ -58,7 +65,21 @@ const ChartView = (props: any) => {
       d.smaVolume70 = c;
     })
     .accessor((d: any) => d.smaVolume70);
-  const calculatedData = ema20(ema50(smaVolume70(initialData)));
+
+  const fi = forceIndex()
+    .merge((d: any, c: any) => {
+      d.fi = c;
+    })
+    .accessor((d: any) => d.fi);
+
+  const fiEMA13 = ema()
+    .id(1)
+    .options({ windowSize: 13, sourcePath: 'fi' })
+    .merge((d: any, c: any) => {
+      d.fiEMA13 = c;
+    })
+    .accessor((d: any) => d.fiEMA13);
+  const calculatedData = ema20(ema50(smaVolume70(fiEMA13(fi(initialData)))));
 
   const xScaleProvider = discontinuousTimeScaleProvider.inputDateAccessor((d: any) => d.date);
   const { data, xScale, xAccessor, displayXAccessor } = xScaleProvider(calculatedData);
@@ -68,10 +89,10 @@ const ChartView = (props: any) => {
   const xExtents = [start, end];
   return (
     <ChartCanvas
-      height={650}
+      height={800}
       ratio={ratio}
       width={width}
-      margin={{ left: 50, right: 50, top: 70, bottom: 30 }}
+      margin={{ left: 70, right: 70, top: 20, bottom: 30 }}
       type={type}
       seriesName="MSFT"
       data={data}
@@ -81,72 +102,17 @@ const ChartView = (props: any) => {
       xExtents={xExtents}
     >
       <Chart
-        id={2}
-        yExtents={[(d: any) => d.volume, smaVolume70.accessor()]}
-        height={150}
-        origin={(w: any, h: any) => [0, h - 150]}
-      >
-        <YAxis axisAt="left" orient="left" ticks={5} tickFormat={format('.2s')} />
-
-        <BarSeries yAccessor={(d: any) => d.volume} fill={(d: any) => (d.close > d.open ? '#6BA583' : '#FF0000')} />
-        <AreaSeries yAccessor={smaVolume70.accessor()} stroke={smaVolume70.stroke()} fill={smaVolume70.fill()} />
-
-        <CurrentCoordinate yAccessor={smaVolume70.accessor()} fill={smaVolume70.stroke()} />
-        <CurrentCoordinate yAccessor={(d: any) => d.volume} fill="#9B0A47" />
-
-        <EdgeIndicator
-          itemType="first"
-          orient="left"
-          edgeAt="left"
-          yAccessor={(d: any) => d.volume}
-          displayFormat={format('.4s')}
-          fill="#0F0F0F"
-        />
-        <EdgeIndicator
-          itemType="last"
-          orient="right"
-          edgeAt="right"
-          yAccessor={(d: any) => d.volume}
-          displayFormat={format('.4s')}
-          fill="#0F0F0F"
-        />
-        <EdgeIndicator
-          itemType="first"
-          orient="left"
-          edgeAt="left"
-          yAccessor={smaVolume70.accessor()}
-          displayFormat={format('.4s')}
-          fill={smaVolume70.fill()}
-        />
-        <EdgeIndicator
-          itemType="last"
-          orient="right"
-          edgeAt="right"
-          yAccessor={smaVolume70.accessor()}
-          displayFormat={format('.4s')}
-          fill={smaVolume70.fill()}
-        />
-      </Chart>
-      <Chart
         id={1}
-        yPan
-        yExtents={[(d: any) => [d.high, d.low], ema20.accessor(), ema50.accessor()]}
-        padding={{ top: 10, bottom: 20 }}
+        height={600}
+        yExtents={(d: any) => [d.high, d.low]}
+        padding={{ top: 10, right: 0, bottom: 20, left: 0 }}
       >
-        <XAxis axisAt="bottom" orient="bottom" />
-        <XAxis axisAt="top" orient="top" flexTicks />
         <YAxis axisAt="right" orient="right" ticks={5} />
+        <XAxis axisAt="bottom" orient="bottom" showTicks={false} outerTickSize={0} />
+        <MouseCoordinateY at="right" orient="right" displayFormat={format('.2f')} />
 
         <CandlestickSeries />
 
-        <LineSeries yAccessor={ema20.accessor()} stroke={ema20.stroke()} highlightOnHover />
-        <LineSeries yAccessor={ema50.accessor()} stroke={ema50.stroke()} highlightOnHover />
-
-        <CurrentCoordinate yAccessor={ema20.accessor()} fill={ema20.stroke()} />
-        <CurrentCoordinate yAccessor={ema50.accessor()} fill={ema50.stroke()} />
-
-        <EdgeIndicator itemType="last" orient="right" edgeAt="right" yAccessor={ema20.accessor()} fill={ema20.fill()} />
-        <EdgeIndicator itemType="last" orient="right" edgeAt="right" yAccessor={ema50.accessor()} fill={ema50.fill()} />
         <EdgeIndicator
           itemType="last"
           orient="right"
@@ -154,39 +120,61 @@ const ChartView = (props: any) => {
           yAccessor={(d: any) => d.close}
           fill={(d: any) => (d.close > d.open ? '#6BA583' : '#FF0000')}
         />
-        <EdgeIndicator itemType="first" orient="left" edgeAt="left" yAccessor={ema20.accessor()} fill={ema20.fill()} />
-        <EdgeIndicator itemType="first" orient="left" edgeAt="left" yAccessor={ema50.accessor()} fill={ema50.fill()} />
-        <EdgeIndicator
-          itemType="first"
-          orient="left"
-          edgeAt="left"
-          yAccessor={(d: any) => d.close}
+        <OHLCTooltip origin={[-40, -10]} />
+      </Chart>
+      <Chart id={2} height={150} yExtents={(d: any) => d.volume} origin={(w: any, h: any) => [0, h - 350]}>
+        <YAxis axisAt="left" orient="left" ticks={5} tickFormat={format('.2s')} />
+        <MouseCoordinateY at="left" orient="left" displayFormat={format('.4s')} />
+
+        <BarSeries
+          yAccessor={(d: any) => d.volume}
           fill={(d: any) => (d.close > d.open ? '#6BA583' : '#FF0000')}
+          opacity={0.5}
         />
+      </Chart>
+      <Chart
+        id={3}
+        height={100}
+        yExtents={fi.accessor()}
+        origin={(w: any, h: any) => [0, h - 200]}
+        padding={{ top: 10, right: 0, bottom: 10, left: 0 }}
+      >
+        <XAxis axisAt="bottom" orient="bottom" showTicks={false} outerTickSize={0} />
+        <YAxis axisAt="right" orient="right" ticks={4} tickFormat={format('.2s')} />
+        <MouseCoordinateY at="right" orient="right" displayFormat={format('.4s')} />
 
-        <MouseCoordinateX at="top" orient="top" displayFormat={timeFormat('%Y-%m-%d')} />
+        <AreaSeries baseAt={(scale: any) => scale(0)} yAccessor={fi.accessor()} />
+        <StraightLine yValue={0} />
+
+        <SingleValueTooltip
+          yAccessor={fi.accessor()}
+          yLabel="ForceIndex (1)"
+          yDisplayFormat={format('.4s')}
+          origin={[-40, 15]}
+        />
+      </Chart>
+      <Chart
+        id={4}
+        height={100}
+        yExtents={fiEMA13.accessor()}
+        origin={(w: any, h: any) => [0, h - 100]}
+        padding={{ top: 10, right: 0, bottom: 10, left: 0 }}
+      >
+        <XAxis axisAt="bottom" orient="bottom" />
+        <YAxis axisAt="right" orient="right" ticks={4} tickFormat={format('.2s')} />
+
         <MouseCoordinateX at="bottom" orient="bottom" displayFormat={timeFormat('%Y-%m-%d')} />
-        <MouseCoordinateY at="right" orient="right" displayFormat={format('.2f')} />
-        <MouseCoordinateY at="left" orient="left" displayFormat={format('.2f')} />
+        <MouseCoordinateY at="right" orient="right" displayFormat={format('.4s')} />
 
-        <OHLCTooltip origin={[-40, -65]} />
-        <MovingAverageTooltip
-          onClick={(e: any) => console.log(e)}
-          origin={[-38, 15]}
-          options={[
-            {
-              yAccessor: ema20.accessor(),
-              type: ema20.type(),
-              stroke: ema20.stroke(),
-              windowSize: ema20.options().windowSize
-            },
-            {
-              yAccessor: ema50.accessor(),
-              type: ema50.type(),
-              stroke: ema50.stroke(),
-              windowSize: ema50.options().windowSize
-            }
-          ]}
+        {/* <AreaSeries baseAt={scale => scale(0)} yAccessor={fiEMA13.accessor()} /> */}
+        <AlternatingFillAreaSeries baseAt={0} yAccessor={fiEMA13.accessor()} />
+        <StraightLine yValue={0} />
+
+        <SingleValueTooltip
+          yAccessor={fiEMA13.accessor()}
+          yLabel={`ForceIndex (${fiEMA13.options().windowSize})`}
+          yDisplayFormat={format('.4s')}
+          origin={[-40, 15]}
         />
       </Chart>
       <CrossHairCursor />
